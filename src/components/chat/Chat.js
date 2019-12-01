c6eabdeade123957dbe67e23635ca0fb07965e34
pbenmoser4 <pbenmoser4@gmail.com>
2019-12-01 00:53:45 -0500

    playing around with the chat input

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
 
 import ChatInputForm from './ChatInputForm';
 
 import { createMessage, getMessages } from '../../actions';
-import { CHAT_FORM } from '../constants';
 
 class Chat extends React.Component {
   componentDidMount = () => {
@@ -13,17 +11,7 @@ class Chat extends React.Component {
   }
 
   onSubmit = formValues => {
-    // console.log(Date.now())
     this.props.createMessage(formValues, Date.now())
-  }
-
-  renderMessageInput = ({input, label, meta}) => {
-    return (
-      <div>
-        <label>{label}</label>
-        <input {...input} autoComplete='off' />
-      </div>
-    );
   }
 
   renderMessages = () => {
@@ -38,22 +26,15 @@ class Chat extends React.Component {
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-          <Field name="text" component={this.renderMessageInput} label="message to..." />
-        </form>
+      <div style={{height: "100%"}}>
         <div className="ui list">
           {this.renderMessages()}
         </div>
+        <div className="ui bottom fixed">
+          <ChatInputForm onSubmit={this.onSubmit} />
+        </div>
       </div>
     );
-  }
-}
-
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.message) {
-    errors.message = "please enter a message";
   }
 }
 
@@ -63,14 +44,9 @@ const mapStateToProps = (state) => {
   }
 }
 
-const formObject = reduxForm({
-  form: CHAT_FORM,
-  validate
-})(Chat);
-
 export default connect(
   mapStateToProps, {
     createMessage,
     getMessages
   }
-)(formObject)
+)(Chat)
--- src/components/chat/ChatInputForm.js
+++ src/components/chat/ChatInputForm.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
+import { CHAT_FORM } from '../constants';
+
 class ChatInputForm extends React.Component {
+  onSubmit = formValues => {
+    this.props.onSubmit(formValues);
+  }
+
+  renderMessageInput = ({input, label, meta}) => {
+    return (
+      <div>
+        <input {...input} autoComplete='off' placeholder="message..."/>
+      </div>
+    );
+  }
+
   render() {
     return(
-      <div>
-        ChatInputForm
+      <div className="ui bottom fixed">
+        <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+          <Field name="text" component={this.renderMessageInput} label="message to..." />
+        </form>
       </div>
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.text) {
+    errors.message = "please enter a message";
+  }
+}
+
+export default reduxForm({
+  form: CHAT_FORM,
+  validate
+})(ChatInputForm);
